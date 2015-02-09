d0b49243b2f9d70a7e3274f52ac131c3ff1773f0
jykke <jyri.lehva@saunalahti.fi>
2015-02-09 21:21:29 +0200

    week 5, exercise 1

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,9 @@ Rails.application.routes.draw do
 
   resource :session, only: [:new, :create, :delete]
 
+  get 'places', to: 'places#index'
+  post 'places', to:'places#search'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
