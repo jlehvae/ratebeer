2eed1587e84441c910ad18968d96879f4d2dc461
jykke <jyri.lehva@saunalahti.fi>
2015-02-10 22:28:29 +0200

    week 5, switched to use the real api and api key instead of the "proxy api"

--- lib/beermapping_api.rb
+++ lib/beermapping_api.rb
@@ -7,7 +7,7 @@ class BeermappingApi
   private
 
   def self.fetch_places_in(city)
-    url = 'http://stark-oasis-9187.herokuapp.com/api/'
+    url = "http://beermapping.com/webservice/loccity/#{key}/"
 
     response = HTTParty.get "#{url}#{ERB::Util.url_encode(city)}"
     places = response.parsed_response["bmp_locations"]["location"]
@@ -18,6 +18,11 @@ class BeermappingApi
     places.inject([]) do | set, place |
       set << Place.new(place)
     end
+  end
+
+  def self.key
+    raise "RATEBEER_APIKEY env variable not defined" if ENV['RATEBEER_APIKEY'].nil?
+    ENV['RATEBEER_APIKEY']
   end
 
 end
