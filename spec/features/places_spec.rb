b6a3b2f33d70083c7f2f6f09a84bdfeddd629692
jykke <jyri.lehva@saunalahti.fi>
2015-02-10 20:31:16 +0200

    week 5, exercise 2, improved tests for places

--- spec/features/places_spec.rb
+++ spec/features/places_spec.rb
@@ -12,4 +12,28 @@ describe "Places" do
 
     expect(page).to have_content "Oljenkorsi"
   end
+
+  it "if two is returned by the API, they are shown at the page" do
+    allow(BeermappingApi).to receive(:places_in).with("kumpula").and_return(
+                                 [ Place.new( name:"Oljenkorsi", id: 1 ),
+                                   Place.new( name:"Koljenorsi", id: 2)]
+                             )
+
+    visit places_path
+    fill_in('city', with: 'kumpula')
+    click_button "Search"
+
+    expect(page).to have_content "Oljenkorsi"
+    expect(page).to have_content "Koljenorsi"
+  end
+
+  it "if nothing is returned by the API, user is told nothing found" do
+    allow(BeermappingApi).to receive(:places_in).with("kumpula").and_return([])
+
+    visit places_path
+    fill_in('city', with: 'kumpula')
+    click_button "Search"
+
+    expect(page).to have_content "No locations in kumpula"
+  end
 end
