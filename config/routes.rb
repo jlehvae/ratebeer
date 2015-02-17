aef42e35724c19fefdf2598b240bb2aa57a33c4e
jykke <jyri.lehva@saunalahti.fi>
2015-02-17 20:58:18 +0200

    week 6, toggle activity

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,9 @@ Rails.application.routes.draw do
 
   resources :beers
 
-  resources :breweries
+  resources :breweries do
+    post 'toggle_activity', on: :member
+  end
 
   root 'breweries#index'
 
